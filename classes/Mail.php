889afc0d61ae95bba4774b14791683349c69a838
Naveenjosh641 <bankapallinaveenjosh@gmail.com>
2025-03-08 08:23:42 +0530

    Update Mail.php

--- classes/Mail.php
+++ classes/Mail.php
@@ -13,15 +13,15 @@ class Mail{
             $mail->isSMTP();
             $mail->Host = 'smtp.gmail.com';
             $mail->SMTPAuth = true;
-            $mail->Username = 'nutrifit.connect@gmail.com';
-            $mail->Password = 'symt oxiq lpqq vdjw';
+            $mail->Username = '';
+            $mail->Password = '';
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
             $mail->Port = 587;
             $mail->isHTML(true);
             
             $otp = rand(100000, 999999);
 
-            $mail->setFrom('nutrifit.connect@gmail.com');
+            $mail->setFrom('');
             $mail->addAddress($remail);
             $mail->Subject = "Enter This OTP Code to Complete Your Signup";
             $mail->Body = "Dear User,<br><br>Thank you for signing up for NutriFit!<br><br>Your one-time password (OTP) for completing your registration is: <strong>$otp</strong><br><br>Please enter this OTP on the signup page to complete your registration.<br><br>If you did not request this, please ignore this email.<br><br>Best regards,<br>The NutriFit Team";
